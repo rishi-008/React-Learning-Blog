7628cf57ae55660d60f7cd4741ee05f7de60a8cd
rishi-008 <67348997+rishi-008@users.noreply.github.com>
2024-07-01 15:38:44 -0400

    added old css to index.css.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,18 +3,20 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faExplosion } from '@fortawesome/free-solid-svg-icons'
 
 export default function Navbar() {
-    return <header>
-        <div className="nav-logo">
-            {/* https://fontawesome.com/icons/explosion?f=classic&s=solid  */}
-            <FontAwesomeIcon icon={faExplosion} />
-            <h1>Journey To Proficiency</h1>
-        </div>
-        <nav>
-            <ul className="navbar-horizontal-display-layout">
-                <i className="fa-solid fa-bars"></i>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="about.html">About Me</a></li>
-            </ul>
-        </nav>
-    </header>
+    return <>
+         <header>
+            <div className="nav-logo">
+                {/* https://fontawesome.com/icons/explosion?f=classic&s=solid  */}
+                <FontAwesomeIcon className="website-icon" icon={faExplosion} />
+                <h1>Journey To Proficiency</h1>
+            </div>
+            <nav>
+                <ul className="navbar-horizontal-display-layout">
+                    <i className="fa-solid fa-bars"></i>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="about.html">About Me</a></li>
+                </ul>
+            </nav>
+        </header>
+    </>
 }
